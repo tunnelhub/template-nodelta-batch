f4b8be249857caf53c2c526fe8217275554fb3fd
Renan William Alves de Paula <renan@4success.com.br>
2021-02-08 14:49:15 -0300

    Update SDK imports

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,7 @@
 import { mocked } from 'ts-jest/utils';
 import * as main from '../src';
-import * as ftp from "basic-ftp";
-import { NoDeltaBatchIntegrationFlow } from "@4success/tunnelhub-sdk/classes/flows/noDeltaBatchIntegrationFlow";
+import * as ftp from 'basic-ftp';
+import { NoDeltaBatchIntegrationFlow } from '@4success/tunnelhub-sdk/src/classes/flows/noDeltaBatchIntegrationFlow';
 import got from 'got';
 
 jest.mock('basic-ftp');
@@ -11,50 +11,52 @@ const mockedFtp = mocked(ftp, true);
 const mockedGot = mocked(got, true);
 
 beforeAll(() => {
-    /**
-     * The code bellow is ** mandatory ** to avoid TunnelHub SDK make external calls trying persist logs
-     * You can make this mock using the same code with any IntegrationFlow at @4success/tunnelhub-sdk/classes/flows 
-     */
-    const persistLogsFunc = jest.spyOn(NoDeltaBatchIntegrationFlow.prototype as any, 'persistLogs');
-    persistLogsFunc.mockImplementation(() => {});
+  /**
+   * The code bellow is ** mandatory ** to avoid TunnelHub SDK make external calls trying persist logs
+   * You can make this mock using the same code with any IntegrationFlow at @4success/tunnelhub-sdk/classes/flows
+   */
+  const persistLogsFunc = jest.spyOn(NoDeltaBatchIntegrationFlow.prototype as any, 'persistLogs');
+  persistLogsFunc.mockImplementation(() => {
+  });
 
-    const updateExecutionStatisticsFunc = jest.spyOn(NoDeltaBatchIntegrationFlow.prototype as any, 'updateExecutionStatistics');
-    updateExecutionStatisticsFunc.mockImplementation(() => {});
+  const updateExecutionStatisticsFunc = jest.spyOn(NoDeltaBatchIntegrationFlow.prototype as any, 'updateExecutionStatistics');
+  updateExecutionStatisticsFunc.mockImplementation(() => {
+  });
 });
 
 
 test('testMyIntegration', async () => {
-    /***
-     * Mocking basic-ftp class and got
-     */
-    //@ts-ignore - is not necessary return all methods
-    mockedFtp.Client.mockImplementation(() => {
-        return {
-            access(options) {
-                //@ts-ignore
-                return new Promise(resolve => resolve({}));
-            },
-            uploadFrom(source, toRemotePath, options) {
-                return new Promise(resolve => {
-                    //@ts-ignore
-                    resolve({});
-                })
-            },
-            close() {
-            }
-        }
-    })
-    
-    //@ts-ignore - is not necessary return all methods
-    mockedGot.mockReturnValue({body: JSON.stringify(require('./data/covidCases.json'))});
+  /***
+   * Mocking basic-ftp class and got
+   */
+  //@ts-ignore - is not necessary return all methods
+  mockedFtp.Client.mockImplementation(() => {
+    return {
+      access(options) {
+        //@ts-ignore
+        return new Promise(resolve => resolve({}));
+      },
+      uploadFrom(source, toRemotePath, options) {
+        return new Promise(resolve => {
+          //@ts-ignore
+          resolve({});
+        });
+      },
+      close() {
+      },
+    };
+  });
 
-    /**
-     * Calling my function
-     */
-    const response = await main.handler({});
-    
-    expect(response.statusCode).toEqual(200);
-    expect(typeof response.body).toBe("string");
+  //@ts-ignore - is not necessary return all methods
+  mockedGot.mockReturnValue({ body: JSON.stringify(require('./data/covidCases.json')) });
 
-    expect(response.body).toEqual('Automation executed with no errors!');
+  /**
+   * Calling my function
+   */
+  const response = await main.handler({});
+
+  expect(response.statusCode).toEqual(200);
+  expect(typeof response.body).toBe('string');
+
+  expect(response.body).toEqual('Automation executed with no errors!');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,167 +1,169 @@
-import { AutomationExecution, IntegrationMessageReturn, Metadata } from "@4success/tunnelhub-sdk";
-import { NoDeltaBatchIntegrationFlow } from "@4success/tunnelhub-sdk/classes/flows/noDeltaBatchIntegrationFlow";
-import { ProxyResult } from "aws-lambda";
+import { AutomationExecution, IntegrationMessageReturn, Metadata } from '@4success/tunnelhub-sdk';
+import { NoDeltaBatchIntegrationFlow } from '@4success/tunnelhub-sdk/src/classes/flows/noDeltaBatchIntegrationFlow';
+import { ProxyResult } from 'aws-lambda';
 import got from 'got';
-import {Parser} from 'json2csv';
-import * as ftp from "basic-ftp";
-import {Readable} from "stream";
+import { Parser } from 'json2csv';
+import * as ftp from 'basic-ftp';
+import { Readable } from 'stream';
 
 /**
  * Integration main type
  */
 type CovidCases = {
-    stateName: string;
-    confirmed: number;
-    recovered: number;
-    deaths: number;
-    updated: string
+  stateName: string;
+  confirmed: number;
+  recovered: number;
+  deaths: number;
+  updated: string
 };
 
 /**
  * This is our entry point for start automation executing.
  * You can personalize the success and error messages according your needs
  * It is mandatory execute the AutomationExecution.executeAutomation method
- * 
- * @param event 
+ *
+ * @param event
  */
 export const handler = async (event: any): Promise<ProxyResult> => {
-    const execution = new Integration(event);
-    await AutomationExecution.executeAutomation(execution);
+  const execution = new Integration(event);
+  await AutomationExecution.executeAutomation(execution);
 
-    if (execution.hasAnyErrors()) {
-        throw Error('Error!');
-    }
-    return {
-        statusCode: 200,
-        body: "Automation executed with no errors!"
-    };
-}
+  if (execution.hasAnyErrors()) {
+    throw Error('Error!');
+  }
+  return {
+    statusCode: 200,
+    body: 'Automation executed with no errors!',
+  };
+};
 
 /**
  * This is our main class that will be responsible to process our automation
  * Basically, you only need to implement the abstract methods and write your business logic
- * 
+ *
  * We provide importante instructions for all methods
  */
 class Integration extends NoDeltaBatchIntegrationFlow {
-    private systems: any[];
+  private systems: any[];
 
+  /**
+   * It is mandatory have the constructor and call the super with event of main handler
+   * You can get the systems configured in automation and save in class property for further use
+   * @param event
+   */
+  constructor(event: any) {
+    super(event);
+    this.systems = event.systems ?? [];
+  }
+
+  /**
+   * Return all columns that will be visible in monitoring screen.
+   * The components order is the display orden in monitoring table
+   *
+   * The implementation of this method is mandatory
+   */
+  defineMetadata(): Metadata[] {
+    return [
+      {
+        fieldName: 'stateName',
+        fieldLabel: 'State name',
+        fieldType: 'TEXT',
+      },
+      {
+        fieldName: 'confirmed',
+        fieldLabel: 'Confirmed cases',
+        fieldType: 'NUMBER',
+      },
+      {
+        fieldName: 'recovered',
+        fieldLabel: 'Recovered cases',
+        fieldType: 'NUMBER',
+      },
+      {
+        fieldName: 'deaths',
+        fieldLabel: 'Deaths',
+        fieldType: 'NUMBER',
+      },
+      {
+        fieldName: 'updated',
+        fieldLabel: 'Last updated at',
+        fieldType: 'DATETIME',
+      },
+    ];
+  }
+
+  /**
+   * Return the source system data as a plain array of objets
+   *
+   * This is the method where you will extract your source data
+   * If your automation is a webhook, the payload sent by caller will be avaiable
+   * in "payload" parameter.
+   *
+   * The implementation of this method is mandatory
+   *
+   * @param payload
+   */
+  async loadSourceSystemData(payload?: any): Promise<CovidCases[]> {
     /**
-     * It is mandatory have the constructor and call the super with event of main handler
-     * You can get the systems configured in automation and save in class property for further use
-     * @param event 
+     * You can get credentials form associated systems with:
+     * const system = this.systems.find(value => value.internalName === 'INTERNAL_SYSTEM_NAME');
      */
-    constructor(event: any) {
-        super(event);
-        this.systems = event.systems ?? [];
+    const sourceSystemData: CovidCases[] = [];
+    const gotResponse = await got(`https://covid-api.mmediagroup.fr/v1/cases?ab=BR`);
+
+    const covidCases = JSON.parse(gotResponse.body);
+    for (const state in covidCases) {
+      if (state === 'All') {
+        continue;
+      }
+      if (covidCases.hasOwnProperty(state)) {
+        sourceSystemData.push({
+          stateName: state,
+          confirmed: covidCases[state].confirmed,
+          recovered: covidCases[state].recovered,
+          deaths: covidCases[state].deaths,
+          updated: covidCases[state].updated,
+        });
+      }
     }
 
-    /**    
-     * Return all columns that will be visible in monitoring screen.
-     * The components order is the display orden in monitoring table
-     * 
-     * The implementation of this method is mandatory
-     */
-    defineMetadata(): Metadata[] {
-        return [
-            {
-                fieldName: 'stateName',
-                fieldLabel: 'State name',
-                fieldType: 'TEXT',
-            },
-            {
-                fieldName: 'confirmed',
-                fieldLabel: 'Confirmed cases',
-                fieldType: 'NUMBER',
-            },
-            {
-                fieldName: 'recovered',
-                fieldLabel: 'Recovered cases',
-                fieldType: 'NUMBER',
-            },
-            {
-                fieldName: 'deaths',
-                fieldLabel: 'Deaths',
-                fieldType: 'NUMBER',
-            },
-            {
-                fieldName: 'updated',
-                fieldLabel: 'Last updated at',
-                fieldType: 'DATETIME',
-            },
-        ]
-    }
+    return sourceSystemData;
+  }
 
+  /**
+   * This routine will be responsible to process your extracted items.
+   * Here is up to you, do your magic :)
+   *
+   * The implementation of this method is mandatory
+   *
+   * @param items
+   */
+  async sendDataInBatch(items: CovidCases[]): Promise<IntegrationMessageReturn> {
     /**
-     * Return the source system data as a plain array of objets
-     * 
-     * This is the method where you will extract your source data
-     * If your automation is a webhook, the payload sent by caller will be avaiable
-     * in "payload" parameter.
-     * 
-     * The implementation of this method is mandatory
-     * 
-     * @param payload 
+     * You can get credentials form associated systems with:
+     * const system = this.systems.find(value => value.internalName === 'INTERNAL_SYSTEM_NAME');
      */
-    async loadSourceSystemData(payload?: any): Promise<CovidCases[]> {
-        /**
-         * You can get credentials form associated systems with:
-         * const system = this.systems.find(value => value.internalName === 'INTERNAL_SYSTEM_NAME');
-         */
-        const sourceSystemData: CovidCases[] = [];
-        const gotResponse = await got(`https://covid-api.mmediagroup.fr/v1/cases?ab=BR`);
+    const props = Object.keys(items[0]);
+    const opts = { fields: props };
 
-        const covidCases = JSON.parse(gotResponse.body);
-        for (const state in covidCases) {
-            if (state === 'All') {
-                continue;
-            }
-            sourceSystemData.push({
-                stateName: state,
-                confirmed: covidCases[state].confirmed,
-                recovered: covidCases[state].recovered,
-                deaths: covidCases[state].deaths,
-                updated: covidCases[state].updated
-            });
-        }
+    const parser = new Parser(opts);
 
-        return sourceSystemData
-    }
+    const client = new ftp.Client();
+    await client.access({
+      host: 'your.ftp.host.com',
+      port: 21,
+      user: 'yourUser',
+      password: 'yourPassword',
+      secure: false,
+    });
 
-    /**
-     * This routine will be responsible to process your extracted items.
-     * Here is up to you, do your magic :)
-     * 
-     * The implementation of this method is mandatory
-     * 
-     * @param items 
-     */
-    async sendDataInBatch(items: CovidCases[]): Promise<IntegrationMessageReturn> {
-        /**
-         * You can get credentials form associated systems with:
-         * const system = this.systems.find(value => value.internalName === 'INTERNAL_SYSTEM_NAME');
-         */
-        const props = Object.keys(items[0]);
-        const opts = {fields: props};
+    const readable = Readable.from([parser.parse(items)]);
+    await client.uploadFrom(readable, `/destinationFolder/covidcases/${Math.floor(Date.now() / 1000)}/extractedData.csv`);
+    client.close();
 
-        const parser = new Parser(opts);
-
-        const client = new ftp.Client();
-        await client.access({
-            host: 'your.ftp.host.com',
-            port: 21,
-            user: 'yourUser',
-            password: 'yourPassword',
-            secure: false,        
-        })
-
-        const readable = Readable.from([parser.parse(items)]);
-        await client.uploadFrom(readable, `/destinationFolder/covidcases/${Math.floor(Date.now() / 1000)}/extractedData.csv`);
-        client.close()
-
-        return {
-            data: {},
-            message: 'Success'
-        }
-    }
+    return {
+      data: {},
+      message: 'Success',
+    };
+  }
 }
