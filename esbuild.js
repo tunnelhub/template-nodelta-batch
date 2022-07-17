cf05e85f1032e290d919b52cb26420080dc2650b
Renan William Alves de Paula <renan@4success.com.br>
2022-07-17 11:04:23 -0300

    Updating build script to handle fargate automatically

--- esbuild.js
+++ esbuild.js
@@ -2,19 +2,47 @@ const glob = require('glob');
 const zip = require('@tybys/cross-zip');
 const path = require('path');
 const esbuildCopy = require('esbuild-plugin-copy');
+const pkg = require('./package.json');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const tunnelhubYml = fs.readFileSync('./tunnelhub.yml').toString('utf8');
+const tunnelhubConfig = yaml.load(tunnelhubYml);
+
+let externals = undefined;
+if (tunnelhubConfig.configuration.runtimeEngine === 'ECS_FARGATE') {
+  externals = [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})];
+}
+
+let targetNodeVersion;
+switch (tunnelhubConfig.configuration.runtime) {
+  case 'nodejs12.x':
+    targetNodeVersion = 'node12';
+    break;
+  case 'nodejs14.x':
+    targetNodeVersion = 'node14';
+    break;
+  case 'nodejs16.x':
+    targetNodeVersion = 'node16';
+    break;
+}
 
 require('esbuild').build({
   entryPoints: glob.sync('./src/**/*.ts'),
   bundle: true,
   minify: true,
   platform: 'node',
-  target: 'node14',
+  target: targetNodeVersion,
   sourcemap: true,
+  external: externals,
   outdir: './build',
   plugins: [
     esbuildCopy.copy({
       assets: {
-        from: ['./tunnelhub.yml'],
+        from: [
+          './tunnelhub.yml',
+          './package.json',
+        ],
         to: ['./build'],
       },
     }),
